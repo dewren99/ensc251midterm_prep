68b500a6d23ca7e152d27fce343a8c891bd21948
Deniz Eren Evrendilek <devrendilek@gmail.com>
2023-07-18 22:47:52 -0700

    remove gitignore

---
+++ Debug/src/dirTree_Test.d
@@ -0,0 +1,8 @@
+src/dirTree_Test.o: ../src/dirTree_Test.cpp ../src/makeDirectoryTree.hpp \
+ ../src/Directory.hpp ../src/TreeNode.hpp
+
+../src/makeDirectoryTree.hpp:
+
+../src/Directory.hpp:
+
+../src/TreeNode.hpp:
